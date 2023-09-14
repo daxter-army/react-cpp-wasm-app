8710d08ce635061e2f85fa021bdb6dd655023212
Mehul Singh Teya <mehulsaini456@gmail.com>
2023-09-14 18:46:28 +0530

    Create dist folder with tunings in index.html

--- cpp_files/main.cpp
+++ cpp_files/main.cpp
@@ -4,17 +4,6 @@
 using namespace std;
 
 int main() {
-    // Notes* NotesStore = new Notes();
-    // NotesStore -> addNote("Buy Milk");
-    // NotesStore -> addNote("Learn WASM");
-    // NotesStore -> addNote("Get a Job, Again !!!");
-    // NotesStore -> addNote("Drink enough water");
-
-    // cout << NotesStore -> totalNotes() << endl;
-    // cout << NotesStore -> getAllNotesAsString() << endl;
-    // NotesStore -> removeNote(1);
-    // NotesStore -> toggleNote(2, true);
-    // cout << NotesStore -> getAllNotesAsString() << endl;
-
+    // Main function call()
     return 0;
 }
